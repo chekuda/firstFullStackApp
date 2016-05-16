d7012888a9feabdb180f7c8ef9270c908ac861a9
Chekuda <sochekilla@gmail.com>
2016-05-16 17:40:16 +0100

    test

---
+++ FrontEnd/modules/basketpage/controllers/basketCtr.js
@@ -0,0 +1,17 @@
+angular.module('demo',['ui.bootstrap'])
+.controller('TabsDemoCtrl', function($scope,$http) {
+  $scope.tabs = [
+    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      $window.alert('You\'ve selected the alert tab!');
+    });
+  };
+
+  $scope.model = {
+    name: 'Tabs'
+  };
+});
