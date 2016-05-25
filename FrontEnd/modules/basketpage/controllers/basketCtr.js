06bbed79d591e69e7ae405f01d1b53e2a5ff0899
Chekuda <sochekilla@gmail.com>
2016-05-25 17:11:16 +0100

    controller added for basket

--- FrontEnd/modules/basketpage/controllers/basketCtr.js
+++ FrontEnd/modules/basketpage/controllers/basketCtr.js
@@ -1,39 +1,92 @@
 angular.module('basketCrt',[])
-.controller('defaultValueBasket', function($scope,$http) {
+.controller('defaultValueBasket', function($scope,$http,$window) {
 
+  
+  /************
+    *Initializate variables
+  ***********/
+  $scope.veapps =[];//veapps initialized
+
+  if(!$window.sessionStorage.getItem("veprompt") && !$window.sessionStorage.getItem("vecontact"))//If all the items removed from basket not display the basketTable
+      {
+        $window.sessionStorage.removeItem("veapps");
+      }
+
+  $scope.veprompt = {
+    type: "veprompt",
+    imgURL:"media/basket/vePromptBasket.png",
+    description:"VePrompt. Website overlays",
+    token: "1"
+  };
+   $scope.vecontact = {
+    type: "vecontact",
+    imgURL:"media/basket/veContactBasket.png",
+    description:"VeContactm. Email Basket recovery",
+    token: "1"
+  };
+
+
+/***************
+  *Adding the item into the basket*
+***************/
+ $scope.addveappsToBasket = function(veappfromSessionStorage)
+  {
+    if(veappfromSessionStorage == "veprompt")
+    {
+      $scope.veapps.push($scope.veprompt);
+    }
+    else if(veappfromSessionStorage == "vecontact")
+    {
+      $scope.veapps.push($scope.vecontact);
+    }
+  }
+
+/***********************
+  Delete item from basket
+************************/
+
+  $scope.deleteItem = function(deletingApp)
+  {
+      if(deletingApp == "veprompt")
+      {
+        $window.sessionStorage.removeItem("veprompt");
+      }
+      if(deletingApp == "vecontact")
+      {
+        $window.sessionStorage.removeItem("vecontact");
+      }
+
+      $window.location.reload();
+  }
 
    /***************
-    Initialize previewBox
+    *Displaying basket*
   ***************/
 
-  $scope.jose = function(){
-    $(".stepOneDiv").toggleClass("laterOn");
-    $(".stepTwoDiv").toggleClass("laterOn");
-  }
-  /**************************
-    Menu
-  *************************/
-  $scope.singleModel = 1;
+  $scope.veappsEnable = false;
+  $scope.veAppsSelectedFromSessionStorage = function()
+  {
+    if($window.sessionStorage.getItem("veapps"))
+    {
+      console.log("veapps Saved");
+      $scope.veappsEnable = true;
+      if($window.sessionStorage.getItem("veprompt"))
+      {
+        var veprompt = "veprompt";
+        $scope.addveappsToBasket(veprompt);
+      }
+      if($window.sessionStorage.getItem("vecontact"))
+      {
+        var vecontact = "vecontact";
+        $scope.addveappsToBasket("vecontact");
+      }
 
-  $scope.radioModel = 'Middle';
+    }
+  }();
 
-  // $scope.checkModel = {
-  //   left: false,
-  //   middle: true,
-  //   right: false
-  // };
+ 
 
-  // $scope.checkResults = [];
-
-  // $scope.$watchCollection('checkModel', function () {
-  //   $scope.checkResults = [];
-  //   angular.forEach($scope.checkModel, function (value, key) {
-  //     if (value) {
-  //       $scope.checkResults.push(key);
-  //     }
-  //   });
-  // });
-
+  
 
 
 });
