54f373695ebcd7eeea15f6042b60327825b6d00f
Chekuda <sochekilla@gmail.com>
2016-05-31 14:45:16 +0100

    files relocated

--- BackEnd/modules/users/services/createToken.js
+++
@@ -1,8 +0,0 @@
-var jwt = require("jsonwebtoken");
-var config = require("../../../configuration/config");//Caling the configuration file for secretPassword
-
-exports.tokenCreated = function(user) {
-	return jwt.sign(user, config.superSecret, { //returns a new token
-		
-	});
-}
