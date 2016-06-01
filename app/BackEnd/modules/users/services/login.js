2b7954c7f1aab554f5a95d26ca5cd12f729eaa4d
Chekuda <sochekilla@gmail.com>
2016-06-01 17:11:49 +0100

    nothing to commit

--- app/BackEnd/modules/users/services/login.js
+++ app/BackEnd/modules/users/services/login.js
@@ -13,10 +13,10 @@ exports.login = function(req,res){
 	else{
 		//Conection with the DB
 		var con = mysql.createConnection({
-		  hostname: "eu-cdbr-azure-west-c.cloudapp.net",
-		  user: "bce64f7320c3cb",
-		  password: "30ec7bc5",
-		  database:"acsm_57c4ae9f2742a96"
+		  hostname: "localhost",
+		  user: "checa",
+		  password: "200288",
+		  database:"vebuilde_2"
 		});
 
 		con.connect(function(err){
