5b6d233568c59b57a5f66d182794a80db05cc1b6
Chekuda <sochekilla@gmail.com>
2016-06-24 12:53:36 +0100

    get the user from database to send the token to frontend

--- app/BackEnd/modules/users/services/getUsersfromDB.js
+++ app/BackEnd/modules/users/services/getUsersfromDB.js
@@ -1,33 +1,48 @@
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
-// exports.getUsersDB = function(req,res){
-// 	var con = mysql.createConnection({
-// 		  host: "eu-cdbr-azure-west-c.cloudapp.net",
-// 		  user: "bce64f7320c3cb",
-// 		  password: "30ec7bc5",
-// 		  database:"acsm_57c4ae9f2742a96"
-// 		});
+exports.getUsersDB = function(req,res){
+	var con = mysql.createConnection({
+		  host: "eu-cdbr-azure-west-c.cloudapp.net",
+		  user: "bce64f7320c3cb",
+		  password: "30ec7bc5",
+		  database:"acsm_57c4ae9f2742a96"
+		});
+		//Local connection
+		// var con = mysql.createConnection({
+		//   hostname: "localhost",
+		//   user: "checa",
+		//   password: "200288",
+		//   database:"vebuilde_2"
+		// });
 
-// 	con.connect(function(err){
-// 	  if(err){
-// 	    console.log(err);
-// 	    return;
-// 	  }
-// 	  else
-// 	  {
-// 	  	console.log('Connection established');
-// 	  	con.query("SELECT * FROM users", function(err,rows){
-// 		  if(err) throw err;
+	con.connect(function(err){
+	  if(err)
+	  {
+	    console.log(err);
+	    return;
+	  }
+	  else
+	  {
+	  	console.log('Connection established');
+	  	con.query("SELECT token FROM client WHERE idclient = ?", req.body.clientId, function(err,rows){
+		  if(err) throw err;
 
-// 		  res.json(rows);
-// 		});
+		  if(rows.length != 0)
+		  {
+		  	res.json({sucess: true, msg:"idClient Exist>>>>>>>>>>", token: rows[0].token});
+		  }
+		  else
+		  {
+		  	res.json({sucess: false, msg: "IdClient doesnt exist>>>>>>>"})
+		  }
+		});
 
-// 		con.end(function(err) {
-// 		  // The connection is terminated gracefully
-// 		  // Ensures all previously enqueued queries are still
-// 		  // before sending a COM_QUIT packet to the MySQL server.
-// 		});
-// 	  }
+		con.end(function(err) {
+		  // The connection is terminated gracefully
+		  // Ensures all previously enqueued queries are still
+		  // before sending a COM_QUIT packet to the MySQL server.
+		});
+	  }
 	  
-// 	});
-// };
+	});
+};
