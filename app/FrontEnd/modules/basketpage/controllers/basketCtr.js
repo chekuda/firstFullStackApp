5508dffebc1cca96448fe979a5d4eee23f16fcc8
Chekuda <sochekilla@gmail.com>
2016-06-24 13:04:53 +0100

    token updated added

--- app/FrontEnd/modules/basketpage/controllers/basketCtr.js
+++ app/FrontEnd/modules/basketpage/controllers/basketCtr.js
@@ -1,18 +1,18 @@
 angular.module('basketCrt',['paymentCtr'])
-.factory('valuetoCharge', function(){//Send money to payment
-  var preTokenTotal = 0;
-  return {
-    getValue: function()
-      {
-        return preTokenTotal;  
-      },
-      changeValue: function(value)
-      {
-        preTokenTotal = value;//* 100;//I got the value from the controller and multiply per 100 to get the HKD
-      }
-  };
-})
-.controller('defaultValueBasket', function($scope,$http,$window,$rootScope,valuetoCharge) {
+// .factory('valuetoCharge', function(){//Send money to payment
+//   var preTokenTotal = 0;
+//   return {
+//     getValue: function()
+//       {
+//         return preTokenTotal;  
+//       },
+//       changeValue: function(value)
+//       {
+//         preTokenTotal = value;//* 100;//I got the value from the controller and multiply per 100 to get the HKD
+//       }
+//   };
+// })
+.controller('defaultValueBasket', function($scope,$http,$window,$rootScope) {
 
 
   $scope.veprompt = {
@@ -29,10 +29,11 @@ angular.module('basketCrt',['paymentCtr'])
   };
 
   $scope.veappsEnable = false;//for displaying basket function
-
+  $scope.clientId = {clientId: $window.sessionStorage.getItem("clientId")};
+  $scope.clientDataAfterPay = {clientId: $window.sessionStorage.getItem("clientId"), token: false};
   $scope.totalBasket = 0;//TotalBasket
   $scope.totalToken = 0;
-  $scope.preToken = 0;//I will give this value to totalToken when be redirect from Paypal
+  $scope.preToken = 0;
   $scope.topupOrPay = false;//This will be for display the right button
   
   /************
@@ -44,6 +45,37 @@ angular.module('basketCrt',['paymentCtr'])
       {
         $window.sessionStorage.removeItem("veapps");
       }
+
+
+ /***************
+    *Get number of token of the client*
+  ***************/
+
+  $rootScope.$on("getNumberTokens",function(event)
+   {
+     $http({
+            url: '/api/getClient',
+            method: 'POST',
+            data: $scope.clientId
+            })
+            .success(function (data){
+              $scope.totalToken = data.token;
+              console.log(data);//Number of token of the client from DB
+              })
+            .error(function(err)
+            {
+              console.log(">>>>>>>>>>>>Error getting the client token reminded: "+err);
+            })
+   });
+
+  if($window.sessionStorage.getItem("clientId"))
+  {
+    console.log("There is not clientId into the sessionStorage>>>>");
+  }
+  else
+  {
+    console.log("Problem to access the IDClient from sessionStorage");
+  }
 
    /***************
     *Displaying basket*
@@ -84,10 +116,12 @@ angular.module('basketCrt',['paymentCtr'])
       if(deletingApp == "veprompt")
       {
         $window.sessionStorage.removeItem("veprompt");
+        $window.sessionStorage.removeItem("vePromptTheme");
       }
       if(deletingApp == "vecontact")
       {
         $window.sessionStorage.removeItem("vecontact");
+        $window.sessionStorage.removeItem("veContacTheme")
       }
 
       $window.location.reload();
@@ -105,7 +139,30 @@ angular.module('basketCrt',['paymentCtr'])
     }
     else
     {
-      console.log("Payment Done");
+        $scope.clientDataAfterPay.token = $scope.totalToken - $scope.totalBasket;
+        if($scope.clientDataAfterPay.token<0)
+        {
+          alert("There is a error, please contact us for further information");
+        }
+        else
+        {
+          //Add the token to the client on DB
+        $http({
+        url: '/api/updateToken',
+        method: 'POST',
+        data: $scope.clientDataAfterPay
+        })
+        .success(function (data){
+          console.log(data);//Number of token of the client from DB
+          $scope.deleteItem("veprompt");
+          $scope.deleteItem("vecontact");
+          $window.location = "http://localhost:3000/home";
+          })
+        .error(function(err)
+        {
+          console.log(">>>>>>>>>>>>Error getting the client token reminded: "+err);
+        })
+        }
     }
   }
 
@@ -134,8 +191,8 @@ angular.module('basketCrt',['paymentCtr'])
     ************************/
   $scope.updateTotalTokensPay = function()
   {
-    valuetoCharge.changeValue($scope.preToken);//Update the quantiy of token in factory
-    $rootScope.$emit("getValueAmountOfTokens");//Launch the event to receive by the paymentController
+    // valuetoCharge.changeValue($scope.preToken);//Update the quantiy of token in factory
+    $rootScope.$emit("getValueAmountOfTokens",$scope.preToken);//Launch the event to receive by the paymentController
   }
 
 
